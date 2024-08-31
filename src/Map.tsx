ec4be0bc48ee007efa1890bad07264df46d9f023
federicotoluzzo <federico.toluzzo1@gmail.com>
2024-08-31 21:10:32 +0000

    Don't have a name for this

--- src/Map.tsx
+++ src/Map.tsx
@@ -2,30 +2,40 @@ import {Block, blockType} from "./Block.ts";
 import {Coordinate} from "./Coordinate.ts";
 
 export function Map(size:number, minDistance:number, maxDistance:number){
-    const map:(typeof Block)[][] = [];
+    const map:Block[][] = [];
+    const selection = new Set<Block>();
+
     for (let i = -size; i <= size; i++) {
         map[i] = [];
         for (let j = -size; j <= size; j++) {
             if (i == 0 && j == 0){
-                map[i][j] = Block(new Coordinate(0, 0), blockType.CENTER);
+                map[i][j] = new Block(new Coordinate(0, 0), blockType.CENTER);
             } else if (Math.floor(Math.sqrt(i * i + j * j)) == minDistance || Math.floor(Math.sqrt(i * i + j * j)) == maxDistance){
-                map[i][j] = Block(new Coordinate(0, 0), blockType.EDGE);
+                map[i][j] = new Block(new Coordinate(0, 0), blockType.EDGE);
+                selection.add()
             } else if (Math.floor(Math.sqrt(i * i + j * j)) > minDistance && Math.floor(Math.sqrt(i * i + j * j)) < maxDistance){
-                map[i][j] = Block(new Coordinate(0, 0), blockType.SELECTED);
+                map[i][j] = new Block(new Coordinate(0, 0), blockType.SELECTED);
             } else {
-                map[i][j] = Block(new Coordinate(0, 0), blockType.EMPTY);
+                map[i][j] = new Block(new Coordinate(0, 0), blockType.EMPTY);
             }
         }
     }
 
-    const yaws:number[][] = [];
-
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            if(j > i){
-                continue;
+    for(let i = 0; i < 96/8; i++){
+        let minDelta = 10000;
+        let closest:Block = new Block(new Coordinate(0, 0), blockType.CENTER);
+        for(const block of selection){
+            if(Math.abs(block.getYaw - Math.tan(Math.PI/4/i)) < minDelta){
+                minDelta = Math.abs(block.getYaw - Math.tan(Math.PI/4/i));
+                closest = block;
             }
-            yaws[i][j] = map[i][j].
         }
+        map[closest.location.posY][closest.location.posX].type = blockType.BEACON;
     }
+
+    return (
+        <>
+            
+        </>
+    )
 }
