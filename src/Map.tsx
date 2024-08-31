6a3ae8cb234fc5a19fb31f5194e8ef894ec3a80f
federicotoluzzo <federico.toluzzo1@gmail.com>
2024-08-31 21:56:36 +0000

    I have no clue if this is going to work at all

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,22 +1,26 @@
 import {Block, blockType} from "./Block.ts";
 import {Coordinate} from "./Coordinate.ts";
+import {ReactElement} from "react";
 
 export function Map(size:number, minDistance:number, maxDistance:number){
     const map:Block[][] = [];
     const selection = new Set<Block>();
 
-    for (let i = -size; i <= size; i++) {
+    for (let i = 0; i <= size; i++) {
         map[i] = [];
-        for (let j = -size; j <= size; j++) {
+        for (let j = 0; j <= size; j++) {
+            if(j < i){
+                map[i][j] = new Block(new Coordinate(0, 0), blockType.EMPTY);
+            }
             if (i == 0 && j == 0){
-                map[i][j] = new Block(new Coordinate(0, 0), blockType.CENTER);
+                map[i][j] = new Block(new Coordinate(j, i), blockType.CENTER);
             } else if (Math.floor(Math.sqrt(i * i + j * j)) == minDistance || Math.floor(Math.sqrt(i * i + j * j)) == maxDistance){
-                map[i][j] = new Block(new Coordinate(0, 0), blockType.EDGE);
+                map[i][j] = new Block(new Coordinate(j, i), blockType.EDGE);
                 selection.add()
             } else if (Math.floor(Math.sqrt(i * i + j * j)) > minDistance && Math.floor(Math.sqrt(i * i + j * j)) < maxDistance){
-                map[i][j] = new Block(new Coordinate(0, 0), blockType.SELECTED);
+                map[i][j] = new Block(new Coordinate(j, i), blockType.SELECTED);
             } else {
-                map[i][j] = new Block(new Coordinate(0, 0), blockType.EMPTY);
+                map[i][j] = new Block(new Coordinate(j, i), blockType.EMPTY);
             }
         }
     }
@@ -33,9 +37,30 @@ export function Map(size:number, minDistance:number, maxDistance:number){
         map[closest.location.posY][closest.location.posX].type = blockType.BEACON;
     }
 
+    const blocks:ReactElement[][] = [];
+
+    for (let i = 0; i <= size; i++) {
+        blocks[i] = [];
+        for (let j = 0; j <= size; j++) {
+            if(j >= i){
+                const type = map[i][j].type.toString().toLowerCase();
+                blocks[size + i + 1][size + j + 1] = <div className={"block " + type}></div>;
+                blocks[size + i + 1][size - j + 1] = <div className={"block " + type}></div>;
+                blocks[size + j + 1][size + i + 1] = <div className={"block " + type}></div>;
+                blocks[size + j + 1][size - i + 1] = <div className={"block " + type}></div>;
+                blocks[size - i + 1][size + j + 1] = <div className={"block " + type}></div>;
+                blocks[size - i + 1][size - j + 1] = <div className={"block " + type}></div>;
+                blocks[size - j + 1][size + i + 1] = <div className={"block " + type}></div>;
+                blocks[size - j + 1][size - i + 1] = <div className={"block " + type}></div>;
+            }
+        }
+    }
+
     return (
         <>
-            
+            <div style={{"display": "grid", "gridTemplateColumns": "1fr ".repeat(2 * size + 1)}}>
+                {blocks}
+            </div>
         </>
     )
 }
